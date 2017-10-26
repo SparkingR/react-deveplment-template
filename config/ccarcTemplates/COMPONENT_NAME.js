b2bea8d98f350f117890894055951c39a2f0130c
SparkingR <sparkurumi@gmail.com>
2017-10-26 16:20:00 +0800

    add create-component-app and templates

---
+++ config/ccarcTemplates/COMPONENT_NAME.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './COMPONENT_NAME.scss';
+
+const cx = classNames.bind(styles);
+
+class COMPONENT_NAME extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return <div className={cx('content')}>Content</div>;
+  }
+}
+
+export default COMPONENT_NAME;
---
+++ config/ccarcTemplates/index.js
@@ -0,0 +1,9 @@
+// import { connect } from 'react-redux';
+import COMPONENT_NAME from './COMPONENT_NAME';
+
+// const mapStateToProps = (state, ownProps) => ({});
+
+// const mapDispatchToProps = (dispatch, ownProps) => ({});
+
+// export default connect(mapStateToProps, mapDispatchToProps)(COMPONENT_NAME);
+export default COMPONENT_NAME;
