4d25a61c0670432adeb9c02780111c9bc14ba8d3
SparkingR <sparkurumi@gmail.com>
2017-10-29 20:01:40 +0800

    modify create-component-app templates

--- config/ccarcTemplates/COMPONENT_NAME.js
+++ config/ccarcTemplates/COMPONENT_NAME.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './COMPONENT_NAME.scss';
 
 const cx = classNames.bind(styles);
 
 class COMPONENT_NAME extends Component {
-  static propTypes = {};
+  // static propTypes = {};
 
-  static defaultProps = {};
+  // static defaultProps = {};
 
   // constructor(props) {
   //   super(props);
